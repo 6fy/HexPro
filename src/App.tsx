8e5547caef8bca7df3d72a5cb160d606e6b3175e
6fy <97406109+6fy@users.noreply.github.com>
2022-11-06 03:01:44 +0100

    changes amount of answers and better resizing

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ enum Result {
 }
 
 function App() {
-	
+
 	const [color, setColor] = useState<string>("red");
 
 	const [answers, setAnswers] = useState<string[]>([]);
@@ -21,6 +21,8 @@ function App() {
 	const [correctCounter, setCorrectGuessCount] = useState<number>(0);
 
 	const [currentGuessCount, setCurrentGuessCount] = useState<number>(0);
+
+	const [answersAmount, setAnswersAmount] = useState<number>(3);
 
 	// Functions
 	function randomColor(): string {
@@ -44,36 +46,79 @@ function App() {
 		setCurrentGuessCount(0);
 
 		// Generate new colors
-		generateDefaultColor();
+		generateDefaultColor(false);
+	}
+
+	function changeAmount(amount: number) {
+		let old = answersAmount;
+		setAnswersAmount(amount);
+
+		if (old > amount) {
+			let newList = answers;
+			newList.splice(newList.indexOf(color), 1);
+			newList.splice(Math.floor(Math.random() * newList.length), 1);
+
+			newList.push(color);
+			setAnswers(newList);
+			return;
+		}
+
+		let newList = answers;
+		newList.push(randomColor());
+		setAnswers(newList);
 	}
 
 	// Automatically runs
-	function generateDefaultColor() {
+	function generateDefaultColor(silent: boolean) {
 		let color = randomColor();
 		setColor(color);
 
-		let colorList = [color, randomColor(), randomColor()];
+		// Add to list
+		let colorList = [color];
+
+		for (let i = 0; i < (answersAmount - 1); i++) {
+			colorList.push(randomColor());
+		}
+
 		// Shuffle the colorList list
 		setAnswers(colorList.sort(() => Math.random() - 0.5));
 	}
 
-	useEffect(() => { generateDefaultColor() }, []);
+	useEffect(() => { generateDefaultColor(false) }, []);
 
 	return (
 		<div className="App">
 			<div id="resizable" className="guess-color-box-holder">
 				<div className="guess-color-box" style={{ background: color }}>
 				</div>
+
+				<div className="change-amount-holder">
+					{answersAmount > 2 &&
+						<button onClick={() => changeAmount(answersAmount - 1)}>-</button>
+					}
+					{answersAmount <= 2 &&
+						<button className="unclickable">-</button>
+					}
+
+					<p>{answersAmount} Answers</p>
+
+					{answersAmount < 6 &&
+						<button onClick={() => changeAmount(answersAmount + 1)}>+</button>
+					}
+					{answersAmount >= 6 &&
+						<button className="unclickable">+</button>
+					}
+				</div>
 			</div>
 			
 			<div className="guess-buttons-holder">
 				<div className="current-guess-counter">
-					<p>You have guessed { correctCounter }/{ guessCount } times correctly!</p>
+					<p>You guessed { correctCounter }/{ guessCount } correctly!</p>
 				</div> 
 
 				{currentGuessCount != 0 && 
 					<div className="current-guess-counter">
-						<p>You tried to guess this color { currentGuessCount } times!</p>
+						<p>You tried to guess this color { currentGuessCount } time{ currentGuessCount <= 1 ? "" : "s" }!</p>
 					</div> 
 				}
 
@@ -87,7 +132,7 @@ function App() {
 
 				{ result === Result.Incorrect && 
 					<div className="incorrect-answer">
-						{ incorrectAnswer } is an incorrect answer!
+						{ incorrectAnswer } incorrect!
 					</div> 
 				}
 				{ result === Result.Correct && 
